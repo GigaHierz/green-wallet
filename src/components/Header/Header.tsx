813bf25265c24b45b6d70db1eb672c08067c409d
Sam Ceja <sammmceja@gmail.com>
2023-05-13 11:14:31 -0700

    create connect wallet button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
-    
   return (
     <header className="mt-3 py-1">
       <div className="container">
-        <p className="float-start mb-1">
-          <Link to="/">
-            Safe Space
-          </Link>
-        </p>
-        <p className="float-end mb-1">
-          <Link to="/wallet/create">
-            Wallet
-          </Link>
-        </p>
+        <div className="flex justify-between items-center">
+          <p className="mb-1">
+            <Link to="/">Safe Space</Link>
+          </p>
+          <div className="flex items-center">
+            <p className="mr-3 mb-1">
+              <Link to="/wallet/create">Wallet</Link>
+            </p>
+            <p className="mb-1">
+              <Link
+                to="/create-wallet"
+                className="px-4 py-2 rounded bg-green-400 ml-4"
+              >
+                Connect Wallet
+              </Link>
+            </p>
+          </div>
+        </div>
       </div>
     </header>
   );
