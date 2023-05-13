728bdfc00f1fda5b184046a0c248d08a9cf01477
Sam Ceja <sammmceja@gmail.com>
2023-05-13 16:38:42 -0700

    landing page setup

--- src/scenes/Products/EventsList.tsx
+++ src/scenes/Products/EventsList.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { SAMPLE_EVENTS } from '../../models/Product'
-import EventCard from './EventCard'
-
+import React from "react";
+import { SAMPLE_EVENTS } from "../../models/Product";
 
 function EventsList() {
   return (
-    <div className='EventsList container card shadow my-5 p-5'>
-        <h1 className='text-center mb-3'>Events</h1>
-        <div className="container">
-
-          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            {SAMPLE_EVENTS.map(event => <EventCard key={event.title} event={event} />)}
-          </div>
-        </div>
+    <div className="EventsList container card bg-violet-400 shadow my-5 p-5">
+      <h1 className="font-mono font-bold  text-center text-7xl">
+        Green Wallet
+      </h1>
+      <div>
+        <p className=" font-bold text-2xl">
+          An ecofriendly, chain-agnostic wallet that accounts for your
+          blockchain transactions by offsetting your carbon footprint.
+        </p>
+      </div>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default EventsList
+export default EventsList;
