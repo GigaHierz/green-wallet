8909bc8c68dab8cab0c496765ffd436350df3a61
Sam Ceja <sammmceja@gmail.com>
2023-05-13 11:29:11 -0700

    add  logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "./greenLOGO.png";
 
 function Header() {
   return (
     <header className="mt-3 py-1">
       <div className="container">
         <div className="flex justify-between items-center">
-          <p className="mb-1">
-            <Link to="/">Safe Space</Link>
-          </p>
+          <div className="flex items-center">
+            <img src={logo} alt="Logo" className="h-10 mr-2" />
+            <p className="mb-1">
+              <Link to="/">Safe Space</Link>
+            </p>
+          </div>
           <div className="flex items-center">
             <p className="mr-3 mb-1">
               <Link to="/wallet/create">Wallet</Link>
