d99f407121bae19a7ba867ccda28d21e2b1dec25
Sam Ceja <sammmceja@gmail.com>
2023-05-13 17:59:18 -0700

    add landing page info + header tabs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "./greenLOGO.png";
+import logo from "./walletLOGO.png";
 
 function Header() {
   return (
@@ -9,13 +9,16 @@ function Header() {
         <div className="flex justify-between items-center">
           <div className="flex items-center">
             <img src={logo} alt="Logo" className="h-10 mr-2" />
+          </div>
+          <div className="flex items-center">
             <p className="mb-1">
               <Link to="/">Safe Space</Link>
             </p>
-          </div>
-          <div className="flex items-center">
             <p className="mr-3 mb-1">
               <Link to="/wallet/create">Wallet</Link>
+            </p>
+            <p className="mb-1">
+              <Link to="/wallet/manage">Manage</Link>
             </p>
             <p className="mb-1">
               <Link
--- src/scenes/Products/EventsList.tsx
+++ src/scenes/Products/EventsList.tsx
@@ -5,13 +5,28 @@ function EventsList() {
   return (
     <div className="EventsList container card bg-violet-400 shadow my-5 p-5">
       <h1 className="font-mono font-bold  text-center text-7xl">
-        Green Wallet
+        Green Wallet🌿
       </h1>
       <div>
-        <p className=" font-bold text-2xl">
+        <p className="font-bold text-2xl m-10 px-56">
           An ecofriendly, chain-agnostic wallet that accounts for your
-          blockchain transactions by offsetting your carbon footprint.
+          blockchain transactions by offsetting your carbon footprint. 👣
         </p>
+        <div className="px-20">
+          <h1 className="font-bold text-3xl mb-10">How it works?</h1>
+          <ul className="text-lg font-semibold">
+            <li>
+              • Sign up for a Gnosis safe wallet Optionally authenticate with
+              Lens
+            </li>
+            <li>• Choose a payment model</li>
+            <li>
+              • Have your carbon footprint be offset automatically every 500 txs
+            </li>
+            <li>• Receive a membership NFT that can be displayed in your</li>
+            <li>• Get analytics about your carbon footprint generation</li>
+          </ul>
+        </div>
       </div>
       <div className="container">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"></div>
